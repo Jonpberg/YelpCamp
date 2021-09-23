45320625ac8a9f73271bd58fc79bab6f6358acd7
Jon <jonpberg9@gmail.com>
2021-09-23 10:26:07 -0400

    Further built out basic styling and added description, price, and image to seeded campgrounds

--- seeds/index.js
+++ seeds/index.js
@@ -21,9 +21,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for(let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Fusce pellentesque consequat sapien sed varius. Integer in erat convallis mauris interdum elementum quis et orci. Phasellus dui ipsum, facilisis in varius vel, sagittis quis felis. Donec cursus ipsum id ligula luctus, id tristique eros posuere. Aenean consequat vitae felis at gravida. Suspendisse potenti.',
+            price
         });
         await camp.save();
     }
