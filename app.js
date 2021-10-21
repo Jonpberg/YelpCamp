f477aa7fd94b460a36d5582e084a2159bdbb9b7e
Jon <jonpberg9@gmail.com>
2021-10-21 11:54:12 -0400

    Added Joi server side schema validation to the new campground route.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground'); 
 const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -54,7 +55,22 @@ app.get('/campgrounds/:id', catchAsync(async (req, res) => {
 }));
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    // if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string().required(),
+            location: Joi.string().required(),
+            description: Joi.string().required()
+        }).required()
+    });
+    const { error } = campgroundSchema.validate(req.body);
+    if(error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    console.log(result);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`); 
