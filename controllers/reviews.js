7c5095c4df4c1ab176c36625828a8fedb313905a
Jon <jonpberg9@gmail.com>
2022-05-03 13:22:29 -0400

    Added review logic so you can no longer review your own campsite, or place multiple reviews on a single campsite.

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -3,14 +3,30 @@ const Review = require('../models/review');
 
 module.exports = {
     createReview: async (req, res) => {
-        const campground = await Campground.findById(req.params.id);
+        const campground = await Campground.findById(req.params.id).populate('reviews');
         const review = new Review(req.body.review);
+        let alreadyReviewed = false;
         review.author = req.user._id;
-        campground.reviews.push(review);
-        await review.save();
-        await campground.save();
-        req.flash('success', `You added a new review to ${campground.title}!`);
-        res.redirect(`/campgrounds/${campground._id}`);
+        campground.reviews.forEach((loopedReview) => {
+            if(loopedReview.author.equals(review.author)) {
+                alreadyReviewed = true;
+            }
+        });
+        if (alreadyReviewed) {
+            req.flash('error', `You already left a review for this Campground.`);
+            res.redirect(`/campgrounds/${campground._id}`);
+        }
+        else if(review.author.equals(campground.author)) {
+            req.flash('error', `You cannot review your own Campground.`);
+            res.redirect(`/campgrounds/${campground._id}`);
+        }
+        else {
+            campground.reviews.push(review);
+            await review.save();
+            await campground.save();
+            req.flash('success', `You added a new review to ${campground.title}!`);
+            res.redirect(`/campgrounds/${campground._id}`);
+        }
     },
     destroyReview: async (req, res) => {
         const { id, reviewId } = req.params;
--- schemas.js
+++ schemas.js
@@ -15,7 +15,7 @@ const reviewSchema = Joi.object({
         rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required()
     }).required()
-})
+});
 
 module.exports.campgroundSchema = campgroundSchema;
 module.exports.reviewSchema = reviewSchema;
